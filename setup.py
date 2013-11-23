abe767d239c5b8e592af226d8feeeff0d10bf54b
JNK <jannik.weyrich@particulate.me>
2013-11-23 02:15:42 +0100

    added stuff for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='designernews_api',
+    packages=['designernews_api'], # this must be the same as the name above
+    version='0.1',
+    description='Python API for Designer News.',
+    author='Jannik Weyrich',
+    author_email='jannikweyrich@gmail.com',
+    url='https://github.com/j7nn7k/designernews_api', # use the URL to the github repo
+    download_url='https://github.com/j7nn7k/designernews_api/tarball/0.1', # I'll explain this in a second
+    keywords=['designer news', 'designernews', 'designernews api', 'designer news api', 'designernews_api'],
+    classifiers=[],
+)
